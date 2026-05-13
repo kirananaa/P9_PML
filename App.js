679f7443df46127fdfb4676497f3f349d9551ae9
clara905 <clararagil09@gmail.com>
2026-05-13 19:16:25 +0700

    fix

--- App.js
+++ App.js
@@ -1,22 +1,14 @@
-<<<<<<< HEAD
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { ActivityIndicator, View } from "react-native";
 
 import AuthProvider, { useAuth } from "./src/contexts/AuthContext";
 
-=======
-import { Text, View } from "react-native";
-
-// Import semua komponen yang dicurigai
-import { AuthProvider } from "./src/contexts/AuthContext";
->>>>>>> 7ec5625c764bbdae136a4bc30ab54107125ca3ef
 import ForgotPasswordScreen from "./src/screens/ForgotPasswordScreen";
 import HomeScreen from "./src/screens/HomeScreen";
 import LoginScreen from "./src/screens/LoginScreen";
 import RegisterScreen from "./src/screens/RegisterScreen";
 
-<<<<<<< HEAD
 const Stack = createStackNavigator();
 
 function AuthStack() {
@@ -62,65 +54,3 @@ export default function App() {
     </AuthProvider>
   );
 }
-=======
-export default function App() {
-  // Fungsi untuk mengecek isi komponen yang ditangkap oleh React
-  const cekStatus = (komponen) => {
-    if (komponen === undefined) return "UNDEFINED ❌ (Ini bikin error!)";
-    if (typeof komponen === "object")
-      return "OBJECT ⚠️ (Bisa error kalau bukan module default)";
-    if (typeof komponen === "function") return "FUNCTION ✅ (Aman)";
-    return typeof komponen;
-  };
-
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "flex-start",
-        padding: 30,
-        backgroundColor: "#fff",
-      }}
-    >
-      <Text
-        style={{
-          fontSize: 24,
-          fontWeight: "bold",
-          marginBottom: 30,
-          alignSelf: "center",
-        }}
-      >
-        🕵️ Hasil Detektif:
-      </Text>
-
-      <Text style={{ fontSize: 16, marginBottom: 15 }}>
-        1. AuthProvider : {cekStatus(AuthProvider)}
-      </Text>
-      <Text style={{ fontSize: 16, marginBottom: 15 }}>
-        2. LoginScreen : {cekStatus(LoginScreen)}
-      </Text>
-      <Text style={{ fontSize: 16, marginBottom: 15 }}>
-        3. RegisterScreen : {cekStatus(RegisterScreen)}
-      </Text>
-      <Text style={{ fontSize: 16, marginBottom: 15 }}>
-        4. HomeScreen : {cekStatus(HomeScreen)}
-      </Text>
-      <Text style={{ fontSize: 16, marginBottom: 15 }}>
-        5. ForgotPwScreen: {cekStatus(ForgotPasswordScreen)}
-      </Text>
-
-      <Text
-        style={{
-          marginTop: 40,
-          color: "red",
-          textAlign: "center",
-          fontWeight: "bold",
-        }}
-      >
-        Perhatikan yang tulisannya ❌ atau ⚠️
-      </Text>
-    </View>
-  );
-}
->>>>>>> 7ec5625c764bbdae136a4bc30ab54107125ca3ef
